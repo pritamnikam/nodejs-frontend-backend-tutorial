791bfaecc889ae46d5d844cb7a733c2fe38d102a
Pritam Nikam <pritam.nikam@gmail.com>
2023-08-27 06:58:42 +0530

    Sending emails

--- src/order/literners/order.listener.ts
+++ src/order/literners/order.listener.ts
@@ -2,12 +2,14 @@ import { Injectable } from "@nestjs/common";
 import { OnEvent } from "@nestjs/event-emitter";
 import { Order } from "../order";
 import { RedisService } from "src/shared/redis.service";
+import { MailerService } from "@nestjs-modules/mailer";
 
 @Injectable()
 export class OrderListener {
 
     constructor(
-        private readonly redisService: RedisService
+        private readonly redisService: RedisService,
+        private readonly mailerService: MailerService
     ) {}
 
     @OnEvent('order.completed')
@@ -18,5 +20,17 @@ export class OrderListener {
             order.ambassador_revenue,
             order.user.name
         );
+
+        await this.mailerService.sendMail({
+            to: 'admin@admin.com',
+            subject: 'Order has been completed',
+            html: `Order #${order.id} with total of $${order.total} has been completed.`
+        });
+
+        await this.mailerService.sendMail({
+            to: order.ambassador_email,
+            subject: 'Order has been completed',
+            html: `You earned $${order.ambassador_revenue} from the link #${order.code}.`
+        });
     }
 }
--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -8,13 +8,27 @@ import { OrderItemService } from './order-item.service';
 import { SharedModule } from 'src/shared/shared.module';
 import { LinkModule } from 'src/link/link.module';
 import { ProductModule } from 'src/product/product.module';
-
+import { MailerModule } from '@nestjs-modules/mailer';
+// import { StripeModule } from 'nestjs-stripe';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Order, OrderItem]),
     SharedModule,
     LinkModule,
     ProductModule,
+    // StripeModule.forRoot({
+    //   apiKey: 'my_secret_key',
+    //   apiVersion: '2020-08-27',
+    // }),
+    MailerModule.forRoot({
+      transport: {
+        host: 'docker.for.mac.localhost',
+        port: 1025,
+      },
+      defaults: {
+        from:'"nest-modules" <modules@nestjs.com>',
+      },
+    }),
   ],
   controllers: [OrderController],
   providers: [OrderService, OrderItemService],
